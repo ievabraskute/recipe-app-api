1f5648653d6783f8825c1dbd8533aa1475f30154
ieva <i.braskute@gmail.com>
2021-01-14 14:30:53 +0200

    add static_root

--- app/app/settings.py
+++ app/app/settings.py
@@ -125,6 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "app" / "static"]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
 AUTH_USER_MODEL = 'core.User'
